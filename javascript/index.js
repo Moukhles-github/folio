b337388040001fafc0e0788d8988761d4f334a9d
Moukhles-github <moukhles_nicolas@icloud.com>
2021-12-13 21:22:21 -0500

    js function v0.1 and modal addition

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,58 @@
+$( document ).ready(function() {
+    
 
+    $('.dropdown-content > li > a').on('click', function(){
+      $("html, body").animate({
+       scrollTop: $($.attr(this, 'href')).offset().top
+     },500);
+    }) 
+ 
+    $('.title-arrow-img a').on('click', function(){
+      $("html, body").animate({
+       scrollTop: $($.attr(this, 'href')).offset().top
+     },500);
+    }) 
+ 
+ 
+    $('.title-arrow-img a').on('click', function(){
+      $("html, body").animate({
+       scrollTop: $($.attr(this, 'href')).offset().top
+     },500);
+    }) 
+    
+    $('.btn-container > button > a').on('click', function(){
+      $("html, body").animate({
+       scrollTop: $($.attr(this, 'href')).offset().top
+     },500);
+    }) 
+ 
+ // Get the modal
+ var modal = document.getElementById("myModal");
+ 
+ // Get the button that opens the modal
+ var btn = document.getElementById("btn-resume");
+ 
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+ 
+ // When the user clicks the button, open the modal 
+ btn.onclick = function() {
+   modal.style.display = "block";
+ }
+ 
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function() {
+   modal.style.display = "none";
+ }
+ 
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
+ 
+ 
+ 
+ });
   
